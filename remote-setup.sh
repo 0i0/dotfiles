0d309e922c01a7ea828fe26753c268fd7274db84
0i0 <liorhakim@gmail.com>
2021-03-03 07:39:52 +0200

    paths

--- remote-setup.sh
+++ remote-setup.sh
@@ -4,8 +4,8 @@ CMD="curl -#L"
 if [ -z "$CMD" ]; then
   echo "No curl or wget available. Aborting."
 else
-  echo "Installing dotfiles-server"
-  mkdir -p "$HOME/dotfiles-server" && \
+  echo "Installing dotfiles"
+  mkdir -p "$HOME/dotfiles" && \
   eval "$CMD https://github.com/0i0/dotfiles/tarball/master | tar -xzv -C ~/dotfiles --strip-components=1 --exclude='{.gitignore}'"
-  . "$HOME/dotfiles-server/setup.sh"
+  . "$HOME/dotfiles/setup.sh"
 fi
